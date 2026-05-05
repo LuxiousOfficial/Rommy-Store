53f7f886ba527483cee80424e98a06e02aa3448f
Rommy Ardiansyah <rommyardiansyah09@gmail.com>
2026-05-05 15:56:18 +0700

    pembaruan model transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-// use Illuminate\Database\Eloquent\Concerns\HasAttributes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
