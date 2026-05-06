8f017c7440f09a30f3254048af864448df7df134
Rommy Ardiansyah <rommyardiansyah09@gmail.com>
2026-05-06 14:51:12 +0700

    pembaruan pada bagian withdrawal

--- app/Http/Resources/WithdrawalResource.php
+++ app/Http/Resources/WithdrawalResource.php
@@ -21,7 +21,7 @@ class WithdrawalResource extends JsonResource
             'bank_account_name' => $this->bank_account_name,
             'bank_account_number' => $this->bank_account_number,
             'bank_name' => $this->bank_name,
-            'proof' => $this->proff ? asset('storage/' . $this->proof) : null,
+            'proof' => $this->proof ? asset('storage/' . $this->proof) : null,
             'status' => $this->status,
             'created_at' => $this->created_at
         ];
